fceedf56d1e4c81ed583430980ba83f51363abf5
Stewart Alsop <stewartalsopIII@gmail.com>
2025-10-11 17:17:45 -0300

    got everything working so that i can walk around and do it

--- app-expo/audio-recorder/app.config.js
+++ app-expo/audio-recorder/app.config.js
@@ -3,8 +3,16 @@ export default {
     name: 'audio-recorder',
     slug: 'audio-recorder',
     version: '1.0.0',
+    owner: 'stewartalsop',
+    runtimeVersion: { policy: 'appVersion' },
+    updates: {
+      url: 'https://u.expo.dev/a08e8bc4-4263-4bec-9088-fad1dcf0399b',
+    },
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL,
+      eas: {
+        projectId: 'a08e8bc4-4263-4bec-9088-fad1dcf0399b',
+      },
     },
   },
 };
